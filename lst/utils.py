c95020a8261ab98674ac5d45e950c1e45a7e4415
Andriy <andriystremeluk95@gmail.com>
2021-01-21 15:22:12 +0200

    Add function flatten into utils.py

--- lst/utils.py
+++ lst/utils.py
@@ -1,3 +1,9 @@
 # -*- coding: utf-8 -*-
 
 
+def flatten(seq):
+    if hasattr(seq, '__iter__') and not isinstance(seq, (str, bytes, bytearray)):
+        for val in seq:
+            yield from flatten(val)
+    else:
+        yield seq
