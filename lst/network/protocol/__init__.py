4e8178629c7d7fcea9109f3d5d555c6e6f21a60b
Andriy <andriystremeluk95@gmail.com>
2021-01-22 19:39:47 +0200

    Add function fetch into protocol/__init__.py

--- lst/network/protocol/__init__.py
+++ lst/network/protocol/__init__.py
@@ -1,4 +1,31 @@
 # -*- coding: utf-8 -*-
 
-
 __all__ = []
+
+
+from urllib.parse import urlparse
+
+
+import http
+import file
+import data
+
+
+class URLError(OSError):
+    pass
+
+
+def fetch(url, **kwargs):
+    parse_res = urlparse(url)
+    scheme = parse_res.scheme
+    if scheme == 'http' or scheme == 'https':
+        return http.fetch(url, **kwargs)
+    elif scheme == 'file':
+        return file.fetch(url, **kwargs)
+    elif scheme == 'data':
+        return data.fetch(url, **kwargs)
+    else:
+        raise URLError(
+            'unknown url type: %s' % scheme
+        )
+
