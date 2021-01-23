ba941da6fc174b72f410dd5d642f005e46ac4e09
Andriy <andriystremeluk95@gmail.com>
2021-01-23 22:53:20 +0200

    Add item operations into class Parsed

--- lst/parse.py
+++ lst/parse.py
@@ -9,3 +9,15 @@ class Parsed:
         self.url = url
         self._args = kwargs
 
+    def __getitem__(self, name):
+        return self._args[name]
+
+    def __setitem__(self, name, value):
+        self._args[name] = value
+
+    def __contains__(self, name):
+        return name in self._args
+
+    def __delitem__(self, name):
+        del self._args[name]
+
