2001c63f0ff9c0254c63f6713cc18fb39c2f4964
Andriy <andriystremeluk95@gmail.com>
2021-01-23 22:29:08 +0200

    Add function fetch into network.py

--- lst/network.py
+++ lst/network.py
@@ -1,2 +1,22 @@
 # -*- coding: utf-8 -*-
 
+from urllib.parse import urlparse
+
+from parse import Parsed
+
+import requests
+
+
+class URLSchemeError(ValueError):
+    pass
+
+
+def fetch(url, **kwargs):
+    url_parsed = urlparse(url)
+    scheme = url_parsed.scheme
+    if scheme.startswith('http'):
+        method = kwargs.get('method', 'GET')
+        resp = requests.request(method, url, **kwargs)
+        return Parsed(resp.content, resp.url, response=resp)
+    else:
+        raise URLSchemeError('unknown url scheme \'%s\'' % scheme)
