13678ad501ad72c83b83eb2601e610e58a6ea209
Andriy <andriystremeluk95@gmail.com>
2021-01-23 22:47:59 +0200

    Add class Parsed into parse.py

--- lst/parse.py
+++ lst/parse.py
@@ -1,2 +1,11 @@
 # -*- coding: utf-8 -*-
 
+from bs4 import BeautifulSoup as bs
+
+
+class Parsed:
+    def __init__(self, content=b'', url='', **kwargs):
+        self.content = content
+        self.url = url
+        self._args = kwargs
+
